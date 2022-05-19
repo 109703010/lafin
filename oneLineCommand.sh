019d06c0b6757c3e212c7c495ece5b95b1520ea6
109703010 <andyszztzn@gmail.com>
2022-05-19 21:46:09 +0800

    Add some example

--- oneLineCommand.sh
+++ oneLineCommand.sh
@@ -1,2 +1,3 @@
 python3 ./scripts/flist.py --path inputData/images --output ./datasets/images.flist
 python3 ./scripts/flist.py --path inputData/masks --output ./datasets/masks.flist
+python3 test.py --model 3 --checkpoints ./checkpoints
