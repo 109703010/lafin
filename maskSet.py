fa9992f5c91718b45cf3f766a6a185702113d142
109703010 <andyszztzn@gmail.com>
2022-05-20 22:01:46 +0800

    Fix bug

--- maskSet.py
+++ maskSet.py
@@ -1,15 +1,15 @@
 import glob
 
-names = glob.glob("celebA/train/images/*.jpg")
+names = glob.glob("celebA/train/images/img_align_celeba/*.jpg")
 
 f = open('celebA/datasets/trainMasks.flist', 'w')
 for i in range(len(names)):
-    f.write("celebA/train/masks/Fixed_mask.png")
+    f.write("celebA/train/masks/Fixed_mask.png\n")
 f.close()
 
 names = glob.glob("celebA/validation/images/*.jpg")
 
 f = open('celebA/datasets/validMasks.flist', 'w')
 for i in range(len(names)):
-    f.write("celebA/validation/masks/Fixed_mask.png")
+    f.write("celebA/validation/masks/Fixed_mask.png\n")
 f.close()
