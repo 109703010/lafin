741a48722428f78a849cc8e6b527f853fb2ff3f0
109703010 <andyszztzn@gmail.com>
2022-05-30 22:09:57 +0800

    Add README.md

--- mask_Generator.py
+++ mask_Generator.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np
 
 mask = np.zeros((256,256,4), np.uint8)
-mask[150:210, 80:185,:] = 255
-mask[150:210, 80:185,:] = 255
+# mask[150:210, 80:185,:] = 255
+mask[150:, 60:200,:] = 255
+# cv2.imshow('1', mask)
+# cv2.waitKey(0)
 cv2.imwrite('Fixed_mask.png', mask)
--- oneLineTest.sh
+++ oneLineTest.sh
@@ -1,4 +1,3 @@
-python3 ./scripts/preprocess_Testing.py
 python3 ./scripts/flist.py --path inputData/images --output ./datasets/images.flist
 python3 ./scripts/flist.py --path inputData/masks --output ./datasets/masks.flist
 python3 test.py --model 3 --checkpoints ./checkpoints/model
