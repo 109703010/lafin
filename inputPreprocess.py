d4e9ba587d419f7a1e271f9c5719d676e53e2a1f
109703010 <andyszztzn@gmail.com>
2022-06-07 16:58:38 +0800

    Fix bug

--- inputPreprocess.py
+++ inputPreprocess.py
@@ -22,15 +22,22 @@ for file in filenames:
         y2 = y + h + 20
         new_img = img[x1:x2, y1:y2]
         new_img = cv2.resize(img, (256,256))
+        cv2.rectangle(img, (x,y), (x+w, y+h), (0, 255, 0), 2)
     gray = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
     eyes = eye_cascade.detectMultiScale(gray, 1.1, 10)
     y_mask = 0
     for (x, y, w, h) in eyes:
         # cv2.rectangle(img, (x,y), (x+w, y+h), (0, 255, 255), 2)
-        y_mask = max(y_mask, y+h)
+        y1_mask = max(y_mask, y+h)
+    faces = face_cascade.detectMultiScale(gray, 1.2, 10)
+    for (x, y, w, h) in faces:
+        # cv2.rectangle(img, (x,y), (x+w, y+h), (0, 255, 0), 2)
+        x1_mask = x
+        x2_mask = x + w
+        y2_mask = y + h
     mask = np.zeros((256,256,4), np.uint8)
-    mask[y_mask:,:,:] = 255
-    img[y_mask-10:,:,:] = 255
+    mask[y1_mask:y2_mask,x1_mask:x2_mask,:] = 255
+    # img[y_mask-10:,:,:] = 255
     cv2.imwrite(os.path.join(INPUT_PATH,file[:-4]+'.jpg'), new_img)
     cv2.imwrite(os.path.join(OUTPUT_PATH,file[:-4]+'.png'), mask)
     # cv2.imshow('img', img)
